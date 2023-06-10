5d45fb0038442c8b16f99f343d244b359da7a753
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-06-11 00:36:00 +0100

    Completed Assignment 1 - Odd Number Checker

---
+++ Section 2. First Unit Test/Sparky/SparkyMSTestProject/CalculatorMSTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Sparky
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoIntegers_GetCorrectSum()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            int result = calculator.AddNumbers(10, 20);
+
+            // Assert
+            Assert.AreEqual(30, result);
+
+        }
+
+        [TestMethod]
+        public void IsOddNumber_InputOddInteger_GetTrue()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            bool result = calculator.IsOddNumber(3);
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsOddNumber_InputEvenInteger_GetFalse()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            bool result = calculator.IsOddNumber(2);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+    }
+}
---
+++ Section 2. First Unit Test/Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Sparky;
+
+[TestFixture]
+public class CalculatorNUnitTests
+{
+    [Test]
+    public void AddNumbers_InputTwoIntegers_GetCorrectSum()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        int result = calculator.AddNumbers(10, 20);
+
+        // Assert
+        Assert.AreEqual(30, result);
+
+    }
+
+    [TestCase(3, true)]
+    [TestCase(2, false)]
+    public void IsOddNumber_InputInteger_GetTrueIfOddOrFalseIfEven(int input, bool expectedResult)
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        bool result = calculator.IsOddNumber(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    
+}
